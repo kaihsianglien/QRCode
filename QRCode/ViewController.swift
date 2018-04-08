37ef33278ca6ff5a3f5dd99598c776abba4733de
kaihsianglien <klien@usc.edu>
2018-04-07 23:03:17 -0700

    first commit

--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//
 import AVFoundation
 
 class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
@@ -8,16 +8,82 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     var qrCodeFrameView: UIView?
     
+    @IBOutlet weak var linkLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.back)
+        
+        guard let captureDevice = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device")
+            return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+            
+            //initialize AVCaptureMetadataOutput to intercept metadata(QR code) and convert to readable format
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            //
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            //the metadata should be converted via QR .qr type
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            //initial previewlayer and add as sublayer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            //start capturing vedio
+            captureSession.startRunning()
+            
+            //make linkLabel to front to be visible
+            view.bringSubview(toFront: linkLabel)
+            
+            // initialize QR Code frame wi¥hich appears when a code is found
+            qrCodeFrameView = UIView()
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubview(toFront: qrCodeFrameView)
+            }
+            
+        } catch {
+            // catch if error occures and end the application
+            print(error)
+            return
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // check metadataObjects arrays as it should at least has one object of QR code
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            linkLabel.text = "No QR code detected, try adjusting camera position"
+            return
+        }
+        
+        // obtain the 1st metadata object and ignore secondary
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        // if metadata is QR code format, update linkLabel and boundry of QR code shown on screen
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                linkLabel.text = metadataObj.stringValue
+            }
+        }
+    }
 }
 
